0815694270d65771dd51cc7482a807f21a627855
Jai2404 <jaibn1234@gmail.com>
2024-01-19 12:10:01 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,31 @@
-import { useState } from 'react'
-import{Route,Routes} from "react-router-dom"
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Navbar from './components/Navbar'
-import Home from './routes/Home'
-import Politics from './routes/Politics'
-import Entertainment from './routes/Entertainment'
-import Buisness from './routes/Buisness'
-import Sports from './routes/Sports'
-import TopHeadings from './routes/TopHeadings'
-
-
-
+import React, { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar';
+import Home from './routes/Home';
+import Politics from './routes/Politics';
+import Entertainment from './routes/Entertainment';
+import Buisness from './routes/Buisness'; // Corrected the typo in the import
+import Sports from './routes/Sports';
+import TopHeadings from './routes/TopHeadings';
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <div className='App'>
-      <Routes>
- <Route path="/" element={<Home />} />
-<Route path="/top-heading" element={<TopHeadings/>}/>
-<Route path="/politics" element={<Politics/>}/>
-<Route path="/sports" element={<Sports/>}/>
-<Route path="/buisness" element={<Buisness/>}/>
-<Route path="/entertainment" element={<Entertainment/>}/>
-      </Routes>
-    
-    
-    
+      <Navbar />
 
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/top-heading" element={<TopHeadings />} />
+        <Route path="/politics" element={<Politics />} />
+        <Route path="/sports" element={<Sports />} />
+        <Route path="/buisness" element={<Buisness />} />
+        <Route path="/entertainment" element={<Entertainment />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
